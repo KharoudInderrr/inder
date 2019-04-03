fbf1f8cb9fbd1be36160a58f288a4fde3be75b4e
Inderjeet Singh <731909@student.tor.loc>
2019-04-03 11:27:37 -0400

    Commit at 11:27a.m)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Add");
+            Console.WriteLine("Made some changes");
+
         }
     }
 }
